6b4661e656938973cc059411d493e4277b207c96
JosephPumphrey <45857842+JosephPumphrey@users.noreply.github.com>
2019-11-21 14:53:05 +0000

    Placedd in more Code
    Heckin code

--- Minesweeper_NEA/Assets/Solver Algorithm.cs
+++ Minesweeper_NEA/Assets/Solver Algorithm.cs
@@ -51,8 +51,8 @@ public class NewBehaviourScript : MonoBehaviour
                 //Making a matrix showing only the tiles bordering a covered mine
                 if (SeenMatrix[i, j] >= 1 && SeenMatrix[i, j] <= 8) 
                 {
-                    for (int x = -1; x < 1; i++){
-                        for (int y = -1; y < 1; i++)
+                    for (int x = -1; x < 1; x++){
+                        for (int y = -1; y < 1; y++)
                         {
                             try
                             {
@@ -75,23 +75,76 @@ public class NewBehaviourScript : MonoBehaviour
          - In situations where a link is under causational dispute, simply calculate probabilities.
          - select one with the highest probability that is closest to the cursor.
          - If one depends on another outcome, where probability is affected again by another, leave it. A better, less risky angle will reveal itself
+         - Select each one in ascending order of uncertain mines around it.
         */
 
-        // Scanning Iteration
-        for (int i = 0; i < 21; i++)
+        //Only allows the code counting up from 1
+        for (int MineCount = 1; MineCount < 8; MineCount++)
         {
-            for (int j = 0; j < 8; j++)
+
+
+
+
+
+            // Scanning Iteration
+            for (int i = 0; i < 21; i++)
             {
-                if (CheckMatrix[i, j])
+                for (int j = 0; j < 8; j++)
                 {
 
+                    //Finding all Mines Around it 
+                    int NumAvailableMines = MineMatrix[i, j];
+                    for (int x = -1; x < 1; x++)
+                    {
+                        for (int y = -1; y < 1; y++)
+                        {
+                            try
+                            {
+                                //Scans all surrounding mines for 100% Probability
+                                //FILL IN LATER------------------------------------------------------//
+                            }
+                            catch { }
+                        }
+                    }
+                    //New variable gets slotted into if statement
+
+                    if (NumAvailableMines == MineCount)
+                    {
+                        if (CheckMatrix[i, j]) // Cycles through each tile bordering an uncovered mine
+                        {
+
+
+                            // Setting actual number of mines the program needs to consider
+                            int NumUncertainMines = MineMatrix[i, j];
+
+
+
+                            //Scanning all mines around it for being linked
+                            for (int x = -1; x < 1; x++)
+                            {
+                                for (int y = -1; y < 1; y++)
+                                {
+                                    try
+                                    {
+                                        //Trying to find if mines around are linked
+
+                                    }
+                                    catch { }
+                                }
+                            }
 
 
 
 
-
+                        }
+                    }
                 }
             }
+
+
+
+
+        //Finishes Iteration
         }
     }
 }
